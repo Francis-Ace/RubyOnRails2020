9d6654a6c55e027f734219fec0f6be59b3b3ee2b
Francis <francisjosecruz@yahoo.ca>
2020-03-09 01:48:37 -0500

    Genetared a players controller with an index and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'players/index'
+  get 'players/show'
   # get 'teams/index'
   # get 'teams/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -6,8 +8,11 @@ Rails.application.routes.draw do
   # Get / => teams#index
 
   root to: 'teams#index'
+  # get 'teams', to: 'teams#index'
 
-  # Get /teams/:id => teams/show
+  # # Get /teams/:id => teams/show
 
-  get "teams/:id", to: 'teams#show', id: /\d+/, as: 'team' # team_path
+  # get "teams/:id", to: 'teams#show', id: /\d+/, as: 'team' # team_path
+  resources 'teams', only: [:index, :show]
+  # same 2 get on top of it
 end
