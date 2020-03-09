e22b5737f88afa1850f26a19c604b844cb56986d
Francis <francisjosecruz@yahoo.ca>
2020-03-09 04:55:57 -0500

    Completed the routes and views for the coach model

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,13 +1,15 @@
 class CoachesController < ApplicationController
   # GET /coaches
   def index
-    @coaches = Coach.order(:name)
+    @coaches = Coach.includes(:team).order(:name)
   end
   # automatically loads the associated view:
   # app/view/students/index.html.erb
-  
+
   # GET /coaches/:id
   def show
-    @coach
+    @coach = Coach.find(params[:id])
   end
+  # automatically loads the associated view:
+  # app/view/students/show.html.erb
 end
