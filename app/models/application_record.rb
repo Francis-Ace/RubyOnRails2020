93e0cfef8f0cba7034bbf27daf15a5f0caf04601
Francis <francisjosecruz@yahoo.ca>
2020-03-12 03:29:41 -0500

    method for getting a single random rpw frp, AR model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,4 +6,8 @@ class ApplicationRecord < ActiveRecord::Base
     random_offset = rand(number_of_rows_available - size_of_collection)
     self.offset(random_offset).limit(size_of_collection)
   end
+
+  def selt.random
+    random_collection(1).first
+  end
 end
